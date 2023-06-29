d9496e4b9bf484013989d1c1518ebb6fad3f88e5
Alexandr Schiwe <devmac@Alexandrs-MacBook-Air.local>
2023-06-29 11:58:59 +0200

    firstpush

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,41 @@
+// Sidebar function
+
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById('mySidenav').style.width = '250px';
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+  document.getElementById('mySidenav').style.width = '0';
 }
+
+// Open and close window function for portfolio site
+
+const details = document.querySelector('.details');
+const blur = document.querySelector('.blur');
+const openCali = document.querySelector('.california');
+const openShop = document.querySelector('.design-shop');
+const shopDetails = document.querySelector('.detailsShop');
+const closeDetails = document.querySelector('.close-details');
+const closeShop = document.querySelector('.close-shop');
+
+openCali.addEventListener('click', function () {
+  //   console.log('button test');
+  details.classList.remove('hidden');
+  blur.classList.remove('hidden');
+});
+
+openShop.addEventListener('click', function () {
+  shopDetails.classList.remove('hidden');
+  blur.classList.remove('hidden');
+});
+
+closeDetails.addEventListener('click', function () {
+  // console.log('closing test');
+  details.classList.add('hidden');
+  blur.classList.add('hidden');
+});
+
+closeShop.addEventListener('click', function () {
+  shopDetails.classList.add('hidden');
+  blur.classList.add('hidden');
+});
